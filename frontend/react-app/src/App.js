c038668a0d3f6425031b9a720738ea95ca60266c
James Birnie <jkbirnie@gmail.com>
2025-02-26 19:58:46 -0800

    Revert "Merge branch 'react' into react-profile"
    This reverts commit 37589d687f4568ae1a8b9b98d25c4903c031cea2, reversing
    changes made to 5df45c9cfdc4edb1286cc426aebd59332468a510.

--- frontend/react-app/src/App.js
+++ frontend/react-app/src/App.js
@@ -1,28 +1,11 @@
 
 import './App.css';
 
-import React from 'react';
-
-//routing for pages?
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-//Login page
-import Login from'./Login';
-import Home from './Home';
-
-const App = () => {
+function App() {
   return (
-    <Router>
-      <nav>
-        <a href="/login">Login</a> 
-        <a href="/home">Home</a>
-      </nav>
-
-      <Routes>
-        <Route path="/Login" element={<Login/>}></Route>
-        <Route path="/Home" element={<Home/>}></Route>
-      </Routes>
-    </Router>
+    <div >
+     <Profile/>
+    </div>
   );
 }
 
